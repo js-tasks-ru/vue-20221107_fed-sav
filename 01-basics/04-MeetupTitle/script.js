47484d29b9ca5b2cd6b8fa8fe84e7923fd5f3c13
Fedor <fed-sav@yandex.ru>
2022-11-13 15:05:17 +0300

    04-MeetupTitle done

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,25 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const App = defineComponent ( {
+  name: 'app',
+
+  data () {
+    return {
+      id: '',
+      title: '',
+    }
+  },
+
+  watch: {
+    id () {
+      fetchMeetupById(this.id).then((resp) => {
+        this.title = resp.title
+      })
+    }
+  }
+
+});
+
+const app = createApp (App);
+const vm = app.mount('#app');
