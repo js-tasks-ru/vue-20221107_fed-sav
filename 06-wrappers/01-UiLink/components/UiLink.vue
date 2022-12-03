36f08f124b2a9a7736d7d14d374a6c49ff6a464a
Fedor <fed-sav@yandex.ru>
2022-12-03 17:02:50 +0300

    01 UiLink done

--- 06-wrappers/01-UiLink/components/UiLink.vue
+++ 06-wrappers/01-UiLink/components/UiLink.vue
@@ -1,10 +1,23 @@
 <template>
-  <a class="link">Link!</a>
+  <a class="link" v-bind="$attrs" v-if="tag === 'a'">
+    <slot></slot>
+  </a>
+  <router-link class="link" v-bind="$attrs" v-else>
+    <slot></slot>
+  </router-link>
 </template>
 
 <script>
 export default {
   name: 'UiLink',
+
+  props: {
+    tag: {
+      type: String,
+      default: 'RouterLink'
+    },
+    
+  }
 };
 </script>
 
