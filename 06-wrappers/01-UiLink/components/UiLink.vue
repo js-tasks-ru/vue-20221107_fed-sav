683e5f4ae9f8f3283c04f62bc29e6079cdfc4e21
Fedor <fed-sav@yandex.ru>
2022-12-06 21:27:35 +0300

    UiLink corrections

--- 06-wrappers/01-UiLink/components/UiLink.vue
+++ 06-wrappers/01-UiLink/components/UiLink.vue
@@ -1,10 +1,7 @@
 <template>
-  <a class="link" v-bind="$attrs" v-if="tag === 'a'">
+  <component v-bind="$attrs" :is="tag" class="link">
     <slot></slot>
-  </a>
-  <router-link class="link" v-bind="$attrs" v-else>
-    <slot></slot>
-  </router-link>
+  </component>
 </template>
 
 <script>
@@ -13,7 +10,7 @@ export default {
 
   props: {
     tag: {
-      type: String,
+      type: [Object, String, Function],
       default: 'RouterLink'
     },
   }
